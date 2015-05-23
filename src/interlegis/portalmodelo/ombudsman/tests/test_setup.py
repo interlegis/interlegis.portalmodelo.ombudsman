7f7af871f37d60e3bccf86c8155ff2330238dd98
Marcio Mazza <mazza@interlegis.leg.br>
2015-05-23 18:32:43 -0300

    Fix test uninstall

--- src/interlegis/portalmodelo/ombudsman/tests/test_setup.py
+++ src/interlegis/portalmodelo/ombudsman/tests/test_setup.py
@@ -66,10 +66,11 @@ class UninstallTestCase(unittest.TestCase):
     def setUp(self):
         self.portal = self.layer['portal']
         self.qi = self.portal['portal_quickinstaller']
-        self.qi.uninstallProducts(products=[PROJECTNAME])
 
     def test_uninstalled(self):
+        self.qi.uninstallProducts(products=[PROJECTNAME])
         self.assertFalse(self.qi.isProductInstalled(PROJECTNAME))
 
     def test_browserlayer_uninstalled(self):
+        self.qi.uninstallProducts(products=[PROJECTNAME])
         self.assertNotIn(IBrowserLayer, registered_layers())
