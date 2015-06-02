b70b2e691a65ae1da030afe83a49b578393a3b7e
Ramiro Batista da Luz <ramiroluz@gmail.com>
2015-06-02 10:40:48 -0300

    Normalize area title so the Choice value can be used to find respective email address. fix #9

--- src/interlegis/portalmodelo/ombudsman/content/ombudsoffice.py
+++ src/interlegis/portalmodelo/ombudsman/content/ombudsoffice.py
@@ -3,6 +3,8 @@
 from interlegis.portalmodelo.ombudsman.interfaces import IOmbudsOffice
 from five import grok
 from plone.dexterity.content import Container
+from zope.component import queryUtility
+from plone.i18n.normalizer.interfaces import IIDNormalizer
 
 
 class OmbudsOffice(Container):
@@ -13,4 +15,5 @@ class OmbudsOffice(Container):
     def get_emails_for_areas(self):
         """Return a dictionary with areas as keys and emails as values.
         """
-        return dict((i['area'], i['email']) for i in self.areas)
+        return dict((queryUtility(IIDNormalizer).normalize(i['area']),
+                     i['email']) for i in self.areas)
